de56bd46e3fdeeed1367100481b304c35dd28f8e
ahmet <ahmet@WIN-R19I4G9J38U>
2026-03-31 17:43:05 +0300

    Proje dosyası ekle.

---
+++ Belediye_Otomasyonu/Home_Screen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Belediye_Otomasyonu
+{
+    public partial class Home_Screen : Form
+    {
+        public Home_Screen()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Screen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
